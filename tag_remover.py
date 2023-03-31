5c316eb3d169812552bbcef9784add8fac92d1b4
Axel Ahlin <axelahlin.haganas@gmail.com>
2023-03-31 11:04:05 +0200

    Update tag remover

--- tag_remover.py
+++ tag_remover.py
@@ -1,8 +1,32 @@
 import re
+import json
+import wikidata_query
 
 def tag_remover(data):
     p = re.compile(r'<.*?>')
     return p.sub('', data)
+
+
+f = open("nfbb.txt",'r')
+xs = f.read().encode().decode('utf-8').split('<b>')
+data = []
+
+for i in range(1,4):#x in xs:
+    x = xs[i]
+    text = tag_remover(x)
+    words = re.findall(r'\S+', text)
+
+    y = {
+        "text": text,
+        "qid": wikidata_query.get_qid(words[0])
+    }
+
+    data.append(y)
+
+f = open("json_dump.json",'w')
+f.write(json.dumps(data))
+
+    
 
 
 
