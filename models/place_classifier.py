fdc5f719246384009c3c09d62cfe42a9f74df824
Alfred Myrne <alfredmyrneblader@gmail.com>
2023-05-08 14:04:09 +0200

    change get coords

--- models/place_classifier.py
+++ models/place_classifier.py
@@ -1,36 +1,18 @@
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from transformers import AutoTokenizer
 
 # Load the data into a pandas df
-data = pd.read_csv("data.csv")
-train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+data = pd.read_json("json_dump.json")
 
-vectorizer = TfidfVectorizer()
-train_tfidf = vectorizer.fit_transform(train_data['text'])
+data.head()
 
-# transform the test data into a TF-IDF matrix
-test_tfidf = vectorizer.transform(test_data['text'])
-model = LogisticRegression()
-model.fit(train_tfidf, train_data['geographic_loc'])
+kbberttokenizer = "KB/bert-base-swedish-cased"
 
-#evaluation metrics
-predictions = model.predict(test_tfidf)
-accuracy = accuracy_score(test_data['geographic_loc'], predictions)
-precision = precision_score(test_data['geographic_loc'], predictions)
-recall = recall_score(test_data['geographic_loc'], predictions)
-f1 = f1_score(test_data['geographic_loc'], predictions)
+tokenizer = AutoTokenizer.from_pretrained()
 
-print("Accuracy:", accuracy)
-print("Precision:", precision)
-print("Recall:", recall)
-print("F1 score:", f1)
 
-# Predict whether a given word is a geographic place or not
-word = "Absint"
-word_tfidf = vectorizer.transform([word])
-geographic_loc = model.predict(word_tfidf)[0]
+def tokenize(batch):
+    return tokenizer(batch["text"], padding=True, truncation=True)
 
-print(word, "is a geographic place:", geographic_loc)
+
+texts_encoded = data.map(tokenize, batched=True, batch_size=None)
--- scripts/get_coords.py
+++ scripts/get_coords.py
@@ -1,4 +1,5 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
+import requests
 
 
 def get_coords(qid):
@@ -7,23 +8,21 @@ def get_coords(qid):
     # loop through articles if p625 is missing, if none set q0
 
     query = """
-    PREFIX  schema: <http://schema.org/>
-    PREFIX  bd:   <http://www.bigdata.com/rdf#>
-    PREFIX  wdt:  <http://www.wikidata.org/prop/direct/>
-    PREFIX  wikibase: <http://wikiba.se/ontology#>
+PREFIX  schema: <http://schema.org/>
+PREFIX  bd:   <http://www.bigdata.com/rdf#>
+PREFIX  wdt:  <http://www.wikidata.org/prop/direct/>
+PREFIX  wikibase: <http://wikiba.se/ontology#>
 
-    SELECT DISTINCT  (SAMPLE(?DR) AS ?coords)
-    WHERE
-    { ?article  schema:about       ?item ;
-                schema:inLanguage  "en" ;
-                schema:isPartOf    <https://en.wikipedia.org/>
-        FILTER ( ?item = <http://www.wikidata.org/entity/"""+qid+"""> )
-        ?item  wdt:P625  ?DR 
-        SERVICE wikibase:label
-        { bd:serviceParam
-                    wikibase:language  "en"
-        }
+SELECT DISTINCT  (SAMPLE(?DR) AS ?coords)
+WHERE
+{ ?article  schema:about       ?item ;
+    FILTER ( ?item = <http://www.wikidata.org/entity/""" + qid + """> )
+    ?item  wdt:P625  ?DR 
+    SERVICE wikibase:label
+    { bd:serviceParam
+                wikibase:language  "sv"
     }
+}
     """
 
     # create a new SPARQLWrapper object and set the query and endpoint URLd
@@ -55,4 +54,35 @@ def get_coords(qid):
         return result
     else:
         print("No coordinates found for Q-ID:", qid)
+        print(results)
+        return None
+
+
+def get_coords_raw(qid):
+    endpoint_url = "https://query.wikidata.org/sparql"
+
+    try:
+        results = sparql.query().convert()
+    except Exception as e:
+        print("Error:", e)
+        exit()
+
+    if "coords" in results["results"]["bindings"][0]:
+        point = results["results"]["bindings"][0]["coords"]["value"]
+
+        import re
+        regex = r"[^\d\s.]"  # match anything that's not a digit or whitespace
+        # remove everything that matches the regex
+
+        if (point.startswith('http')):
+            print("URL returned. Point: " + point)
+            # This needs to be revised
+            return None
+
+        result = [float(x) for x in re.sub(regex, "", point).split(" ")]
+
+        return result
+    else:
+        print("No coordinates found for Q-ID:", qid)
+        print(results)
         return None
